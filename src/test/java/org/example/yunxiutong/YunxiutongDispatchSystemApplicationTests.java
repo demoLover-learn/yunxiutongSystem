5c2c262e898a918d86cc8c2a897a24ff6272b732
SnTest <3197661591@qq.com>
2026-05-24 14:14:55 +0800

    原版代码

---
+++ src/test/java/org/example/yunxiutong/YunxiutongDispatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yunxiutong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunxiutongDispatchSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
